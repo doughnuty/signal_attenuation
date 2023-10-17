ed2d0decb071839fa37050fc41ace2a5885bc166
doughnut <malika240502@gmail.com>
2023-10-17 22:26:29 +0600

    works with browser

--- wifi/sender_main.py
+++ wifi/sender_main.py
@@ -24,7 +24,7 @@ def show_on_screen(s1):
     
 wlan_ap = network.WLAN(network.AP_IF)
 
-addr = socket.getaddrinfo('0.0.0.0', port)[0][-1]
+addr = ('0.0.0.0', port)
 wlan_ap.active(True)
 
 wlan_ap.config(essid=ap_ssid, password=ap_password, authmode=ap_authmode)
@@ -36,8 +36,12 @@ server_socket.listen(1)
 scl = Pin(18, Pin.OUT, Pin.PULL_UP)
 sda = Pin(17, Pin.OUT, Pin.PULL_UP)
 i2c = SoftI2C(scl=scl, sda=sda, freq=450000)
-oled = ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3c)
-oled.poweron()
+print(i2c.scan())
+try:
+    oled = ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3c)
+    oled.poweron()
+except:
+    print("oled error")
 
 print('Connect to WiFi ssid ' + ap_ssid + ', default password: ' + ap_password)
 print('and access the ESP via your favorite web browser at 192.168.4.1.')
@@ -50,7 +54,10 @@ print('station connected from ', addr)
 while pn < 500:
     send_packet(station, pn)
     print('packet sent: ',pn)
-    show_on_screen(str(pn))
     pn = pn + 1
-    time.sleep(3)
+    try:
+        show_on_screen(str(pn))
+    except:
+        continue
     
+server_socket.close()
