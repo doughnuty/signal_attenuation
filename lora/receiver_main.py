fa9d22d56b161a39af16b72ea343981081e86b32
doughnut <malika240502@gmail.com>
2023-11-14 20:08:32 +0600

    update files

--- lora/receiver_main.py
+++ lora/receiver_main.py
@@ -1,118 +1,44 @@
-from sx1262 import SX1262
-from machine import SoftI2C, Pin, SDCard, UART, RTC
-import ssd1306
+from network import LoRa
+import socket
 import time
-from time import sleep
-import os
 import struct
 
-rst = Pin(21, Pin.OUT)
-rst.value(1)
-scl = Pin(18, Pin.OUT, Pin.PULL_UP)
-sda = Pin(17, Pin.OUT, Pin.PULL_UP)
-i2c = SoftI2C(scl=scl, sda=sda, freq=450000)
-oled = ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3c)
-oled.poweron()
+from machine import SD
+import os
 
-def show_on_screen(s1, s2, s3):
-    oled.fill(0)
-    oled.text(str(s1), 0, 0)
-    oled.text(s2, 0, 10)
-    oled.text(s3, 0, 20)
-    oled.show()
+contype = 'LoRa-'
+lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868)
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setblocking(False)
+i = 0
 
-# SPI pins
-SCK  = 9
-MOSI = 10
-MISO = 11
-# Chip select
-CS   = 8
-# Receive IRQ
-RX   = 14
+# mount sd card
+sd = SD()
+os.mount(sd, '/sd')
+os.listdir('/sd')
 
-RST  = 12
-BUSY = 13
+today = time.localtime()
+spr = "-"
+today_str = spr.join(str(item) for item in today)
+print(today_str)
+f = open('/sd/' + contype + today_str + '.csv', 'a')
 
-# Setup LoRa
-lora = SX1262(1, SCK, MOSI, MISO, CS, RX, RST, BUSY)
-lora.begin(freq=868.5, bw=125.0, sf=10, cr=5, preambleLength=8, implicit=False, implicitLen=0xFF, blocking=False)
+i = 0
+j = 0
 
-# init sd card
-# SPI pins
-SCK_SD  = 38
-MOSI_SD = 33
-MISO_SD = 34
-# Chip select
-CS_SD   = 26
-
-sck=Pin(SCK_SD, Pin.OUT, Pin.PULL_DOWN)
-mosi=Pin(MOSI_SD, Pin.OUT, Pin.PULL_UP)
-miso=Pin(MISO_SD, Pin.IN, Pin.PULL_UP)
-
-cs=Pin(CS_SD, Pin.OUT)
-
-# uncomment when SD card is connected
-os.mount(SDCard(slot=3, sck = sck, miso = miso, mosi = mosi, cs = cs, freq = 8000000), '/sd')
-
-LOGGING_PATH = '/sd/logging'
-
-# function for logging
-def my_print(*args):
-    """ Stores all the prints with timestamp on sd card with LOGGING_PATH
-        and prints in REPL"""
-    global LOGGING_PATH
-    with open(LOGGING_PATH+'.txt', 'a', encoding='utf-8') as file:
-        t = time.localtime()
-        file.write('{}-{}-{} {}:{}:{}\t\t'.format(t[0], t[1], t[2], t[3], t[4], t[5]))
-        for i in args:
-            file.write(str(i)+' ')
-        file.write('\n')
-    print(*args)
-
-# show_on_screen('card initialized', '', '')
-
-show_on_screen('ready', '', '')
-
-recv_pkts = 0
-target_ID = 255
-target_ID_Bytes = target_ID.to_bytes(1, "big")
-# called when received lora packet
-def handler(event):
-    try:
-        if event & SX1262.RX_DONE:
-            
-            global recv_pkts                
-            x, err = lora.recv()
-            if err != 0:
-                print('error: ', err)
-                return
-            print(x)
-            if len(x) > 0:
-                recv_pkts += 1
-                
-                ID, seq, CRC = struct.unpack("!BHI", x)
-                print(ID, seq)
-                print('pkt #:', seq, ' received')
-                show_on_screen(str(seq), '', '')
-                rssi = lora.getRSSI()
-                my_print('pkt #:', seq)
-                print('pkt #:', seq)
-                my_print('rss:', rssi)
-                print('rss:', rssi)
-        elif event & SX1262.TX_DONE:
-            print('sending')
-    except Exception as e:
-        show_on_screen(oled, 'Error occured', '')
-        print('receiver error: ', err)
-        time.sleep(1)
-
-lora.setBlockingCallback(False, handler)
-
-while 1:
-    try:
-        if recv_pkts >= 500:
+while i < 500:
+    rcv = s.recv(4)
+    if rcv:
+        try:
+            packets = struct.unpack("i", rcv)
+        except Exception as e:
+            print(e)
             continue
-        time.sleep(0.5)
-    except Exception as e:
-        show_on_screen(oled, 'Error occured', '')
-        time.sleep(1)
+    
+        st = lora.stats()
+        print(packets[0], ' ', st[1])
+        f.write(str(packets[0]) + ' ' + str(st[1]) + '\n')
+        time.sleep(5)
+        i = packets[0]
+        j = j + 1
+f.close()
--- lora/sender_main.py
+++ lora/sender_main.py
@@ -1,117 +1,19 @@
-from sx1262 import SX1262
-from _sx126x import *
-from machine import SoftI2C, Pin, SDCard, UART, RTC
-import ssd1306
+from network import LoRa
+import socket
 import time
-from time import sleep
-import os
 import struct
-from ubinascii import crc32
 
-rst = Pin(21, Pin.OUT)
-rst.value(1)
-scl = Pin(18, Pin.OUT, Pin.PULL_UP)
-sda = Pin(17, Pin.OUT, Pin.PULL_UP)
-i2c = SoftI2C(scl=scl, sda=sda, freq=450000)
-oled = ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3c)
-oled.poweron()
-
-def show_on_screen(s1, s2, s3):
-    oled.fill(0)
-    oled.text(s1, 0, 0)
-    oled.text(s2, 0, 10)
-    oled.text(s3, 0, 20)
-    oled.show()
-
-# SPI pins
-SCK  = 9
-MOSI = 10
-MISO = 11
-# Chip select
-CS   = 8
-# Receive IRQ
-RX   = 14
-
-RST  = 12
-BUSY = 13
-
-# Setup LoRa
-lora = SX1262(1, SCK, MOSI, MISO, CS, RX, RST, BUSY)
-lora.begin(freq=868.5, bw=125.0, sf=10, cr=5, preambleLength=8, implicit=False, implicitLen=0xFF, blocking=False)
-
-# init sd card
-# SPI pins
-SCK_SD  = 38
-MOSI_SD = 33
-MISO_SD = 34
-# Chip select
-CS_SD   = 26
-
-sck=Pin(SCK_SD, Pin.OUT, Pin.PULL_DOWN)
-mosi=Pin(MOSI_SD, Pin.OUT, Pin.PULL_UP)
-miso=Pin(MISO_SD, Pin.IN, Pin.PULL_UP)
-
-cs=Pin(CS_SD, Pin.OUT)
-
-# uncomment when SD card is connected
-# os.mount(SDCard(slot=3, sck = sck, miso = miso, mosi = mosi, cs = cs, freq = 8000000), '/sd')
-
-LOGGING_PATH = '/sd/logging'
-
-# function for logging
-def my_print(*args):
-    """ Stores all the prints with timestamp on sd card with LOGGING_PATH
-        and prints in REPL"""
-    global LOGGING_PATH
-    with open(LOGGING_PATH+'.txt', 'a', encoding='utf-8') as file:
-        t = time.localtime()
-        file.write('{}-{}-{} {}:{}:{}\t\t'.format(t[0], t[1], t[2], t[3], t[4], t[5]))
-        for i in args:
-            file.write(str(i)+' ')
-        file.write('\n')
-    print(*args)
-
-# show_on_screen('card initialized', '', '')
-
-# show_on_screen('ready', '', '')
-device_ID = 255
-device_ID_bytes = device_ID.to_bytes(1, "big")
-# pack and encrypt
-def prepare_packet(seq):
-    joined = b"".join([device_ID_bytes, seq.to_bytes(2, "big")])
-    CRC = crc32(joined)
-    pkt = struct.pack("!BHI", device_ID, seq, CRC)
-    return pkt
-
-pkt_num = 1
-
-# called when received lora packet
-def handler(event):
-    if event & SX1262.RX_DONE:
-        global recv_pkts
-        x, err = lora.recv()
-        
-        # if len(x) > 0:
-        #     recv_pkts += 1
-        #     my_print(x, 'pkt #:', recv_pkts)
-        #     my_print('rss:', lora.getRSSI())
-    elif event & SX1262.TX_DONE:
-        print('sending')
 time.sleep(10)
-lora.setBlockingCallback(False, handler)
-sleeper = 5 # sleeping time in seconds
-while 1:
-    try:
-        if pkt_num > 500:
-            show_on_screen('done', '', '')
-            break
-        print(f'sending packet #{pkt_num}')
-        msg = prepare_packet(pkt_num)
-        lora.send(msg)
-        show_on_screen(f'{pkt_num}', '', '')
-        pkt_num += 1
-        time.sleep(sleeper)
-    except Exception as e:
-        show_on_screen('Error occured', '', '')
-        print(e)
-        time.sleep(1)
+
+# LoRa setup
+lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868)
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setblocking(False)
+i = 0
+
+while i < 500:
+    packet = struct.pack("i", i)
+    s.send(packet)
+    print('Ping {}'.format(i))
+    i= i+1
+    time.sleep(5)
